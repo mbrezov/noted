43206151b252592521d79f0c1c2608a58ecb5977
mbrezov <mariobrezovecki@gmail.com>
2023-07-31 12:03:47 +0200

    sidebar updated with client side fetch

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -1,9 +1,29 @@
+"use client";
 import styles from "./Sidebar.module.scss";
+import { useState, useEffect } from "react";
 
 const Sidebar = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://noted.pockethost.io/api/collections/Category/records/")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.items);
+      });
+  }, []);
+
+  console.log(data);
+
   return (
     <div className={styles.container}>
       <h1>Kategorije</h1>
+      {data &&
+        data.map((cat) => (
+          <div key={cat.id} className={styles.category}>
+            <div className={styles.category_color}></div>
+            {cat.name}
+          </div>
+        ))}
     </div>
   );
 };
